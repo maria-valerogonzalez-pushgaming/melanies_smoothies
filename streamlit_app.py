0b04bce40b6bd5b41f93ea9cb62c80af8ae00070
Maria Valero Gonzalez <maria.valero-gonzalez@pushgaming.com>
2025-10-14 16:22:56 +0100

    Initialize Snowflake session in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.session import Session 
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -19,7 +20,9 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-session = get_active_session()
+# session = get_active_session()
+session = Session.builder.configs(st.secrets["snowflake"]).create()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
