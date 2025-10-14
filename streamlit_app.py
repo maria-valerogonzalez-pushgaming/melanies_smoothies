75995e33cbadcf9a2aaa27c1ff45cd1145ec56af
Maria Valero Gonzalez <maria.valero-gonzalez@pushgaming.com>
2025-10-14 16:36:16 +0100

    Refactor Smoothiefroot API response handling
    Updated the way the Smoothiefroot API response is displayed.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,6 @@ import streamlit as st
 from snowflake.snowpark.session import Session 
 from snowflake.snowpark.functions import col
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
-
 # Write directly to the app
 st.title(f":cup_with_straw: Customise Your Smoothie :cup_with_straw:")
 st.write(
@@ -57,3 +52,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
